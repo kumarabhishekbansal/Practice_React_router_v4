2ce0bc0d14fada14dc3b8b56b47d36e7406cc223
kumarabhishekbansal <kumarabhishekbansal>
2024-03-15 14:14:37 +0530

    Login Page complete and logout Complete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,84 @@
-import React, { Component,createContext } from "react";
+// import React, { Component,createContext } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Child from "./components/Child";
+// import Child from "./components/Child";
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log("App constructor calling");
+//   }
+//   render() {
+//     return (
+//       <>
+//         <Router>
+//           <div>
+//             <h2>Accounts</h2>
+
+//             <ul>
+//               <li>
+//                 <Link to="/netflix">Netflix</Link>
+//               </li>
+//               <li>
+//                 <Link to="/zillow-group">Zillow Group</Link>
+//               </li>
+//               <li>
+//                 <Link to="/yahoo">Yahoo</Link>
+//               </li>
+//               <li>
+//                 <Link to="/modus-create">Modus Create</Link>
+//               </li>
+//             </ul>
+
+//             <Switch>
+//               <Route path="/:id" children={<Child />} />
+//             </Switch>
+//           </div>
+//         </Router>
+//       </>
+//     );
+//   }
+// }
+
+
+import React, { Component } from 'react'
+import ProvideAuth from "./components/ProvideAuth"
+import AuthButton from "./components/AuthButton"
+import PrivateRoute from "./components/PrivateRoute"
+import PublicPage from "./components/PublicPage"
+import ProtectedPage from "./components/ProtectedPage"
+import LoginPage from "./components/LoginPage"
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("App constructor calling");
-  }
   render() {
     return (
       <>
+        <ProvideAuth>
         <Router>
           <div>
-            <h2>Accounts</h2>
+            <AuthButton />
 
             <ul>
               <li>
-                <Link to="/netflix">Netflix</Link>
+                <Link to="/public">Public Page</Link>
               </li>
               <li>
-                <Link to="/zillow-group">Zillow Group</Link>
-              </li>
-              <li>
-                <Link to="/yahoo">Yahoo</Link>
-              </li>
-              <li>
-                <Link to="/modus-create">Modus Create</Link>
+                <Link to="/protected">Protected Page</Link>
               </li>
             </ul>
 
             <Switch>
-              <Route path="/:id" children={<Child />} />
+              <Route path="/public">
+                <PublicPage />
+              </Route>
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <PrivateRoute path="/protected">
+                <ProtectedPage />
+              </PrivateRoute>
             </Switch>
           </div>
         </Router>
+      </ProvideAuth>
       </>
-    );
+    )
   }
 }
--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-import { AuthContext } from './ProvideAuth'
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { AuthContext } from "./ProvideAuth";
 class AuthButton extends Component {
   render() {
-    let auth=this.context;
+    let auth = this.context;
     return (
-     <>
-        auth.user?(Welcome!{" "}
-        <button
-          onClick={() => {
-            auth.signout(() => this.props.history.push("/"));
-          }}
-        ></button>):(<p>You are not logged in.</p>)
-     </>
-    )
+      <>
+        {auth.user ? (
+          <>
+          <h1>Welcome user</h1>
+            <button
+              onClick={() => {
+                auth.signout(() => this.props.history.push("/"));
+              }}
+            ></button>
+          </>
+        ) : (
+          <p>You are not logged in.</p>
+        )}
+      </>
+    );
   }
 }
 AuthButton.contextType = AuthContext;
