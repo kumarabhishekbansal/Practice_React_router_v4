1388a5c7aadc887fdfbd2406e9710609ca917e67
kumarabhishekbansal <kumarabhishekbansal>
2024-03-15 11:46:03 +0530

    starting login page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Child from './components/Child';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Child from "./components/Child";
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     console.log("App constructor calling");
   }
   render() {
     return (
       <>
- <Router>
-      <div>
-        <h2>Accounts</h2>
+        <Router>
+          <div>
+            <h2>Accounts</h2>
 
-        <ul>
-          <li>
-            <Link to="/netflix">Netflix</Link>
-          </li>
-          <li>
-            <Link to="/zillow-group">Zillow Group</Link>
-          </li>
-          <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/modus-create">Modus Create</Link>
-          </li>
-        </ul>
+            <ul>
+              <li>
+                <Link to="/netflix">Netflix</Link>
+              </li>
+              <li>
+                <Link to="/zillow-group">Zillow Group</Link>
+              </li>
+              <li>
+                <Link to="/yahoo">Yahoo</Link>
+              </li>
+              <li>
+                <Link to="/modus-create">Modus Create</Link>
+              </li>
+            </ul>
 
-        <Switch>
-          <Route path="/:id" children={<Child />} />
-        </Switch>
-      </div>
-    </Router>
+            <Switch>
+              <Route path="/:id" children={<Child />} />
+            </Switch>
+          </div>
+        </Router>
       </>
-    )
+    );
   }
 }
